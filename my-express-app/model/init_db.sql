34e91a076031b4d6f7f07c50bbfaa3e4c108b05a
Rhianwen Beint <rhianwenb@gmail.com>
2024-06-29 14:07:48 +0100

    Initial Express commit

---
+++ my-express-app/model/init_db.sql
@@ -0,0 +1,19 @@
+--
+-- Drop Tables
+--
+
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS ;
+SET foreign_key_checks = 1;
+
+--
+-- Create Tables
+--
+
+CREATE TABLE users(
+
+);
+
+CREATE TABLE properties(
+
+);
