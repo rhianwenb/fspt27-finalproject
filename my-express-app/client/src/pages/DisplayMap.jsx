3104164387727fbc22b4e484c9dcccb15539e549
Clio von Petersdorff <clio.petersdorff@gmail.com>
2024-07-22 20:03:20 +0100

    update to modal

--- my-express-app/client/src/pages/DisplayMap.jsx
+++ my-express-app/client/src/pages/DisplayMap.jsx
@@ -38,24 +38,28 @@ export default function DisplayMap(){
     }
 
 
+
     return (
         <>
         {/* MORE INFORMATION */}
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Launch demo modal
-          </button>
+        <button type="submit" 
+                    className = "btn btn-lg" 
+                    data-bs-toggle="modal" 
+                    data-bs-target="#staticBackdrop"> How to use NextTenant </button>
 
-          <div className="modal-dialog modal-dialog-centered" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-body">
+                          More information
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick = {()=>goToLogin()}>Login</button>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
 
         <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
           <Map style={{width: '100%', height: '60vh'}}
